fb523d063c9f883651664ee1095da552b214aa51
JeremySorensen <jeremy.a.sorensen@gmail.com>
2016-10-03 08:01:08 -0700

    generic, added edge finder, c interface
    now there is a Point trait, and some bounds on x and y (any numeric type
    will work)
    also finds the outside edges, and added a C interface and made it a dll

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,32 @@
 
 use std::collections::HashSet;
 use std::fmt;
+use std::ops::{Mul, Sub};
 
-#[derive(Debug, PartialEq, Clone, Copy)]
-pub struct Point {
-    x: f32,
-    y: f32
-}
-impl Point {
-    pub fn new(x: f32, y: f32) -> Point {
-        Point { x: x, y: y }
-    }
+pub trait Zeroable {
+    fn zero() -> Self;
 }
 
-impl fmt::Display for Point {
+pub trait Point {
+    type T: PartialOrd + PartialEq + Sub + Mul + Zeroable + fmt::Display;
+    fn get_x(&self) -> Self::T;
+    fn get_y(&self) -> Self::T;
+}
+
+impl<U> fmt::Display for Point<T=U> 
+        where U: PartialOrd + PartialEq + Sub + Mul + Zeroable + fmt::Display {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
+        write!(f, "({}, {})", self.get_x(), self.get_y())
     }
 }
+
+impl<U> Point for (U, U) 
+        where U: PartialOrd + PartialEq + Sub + Mul + Zeroable + fmt::Display + Copy {
+    type T = U;
+    fn get_x(&self) -> U { self.0 }
+    fn get_y(&self) -> U { self.1 }
+}
+
 
 #[derive(Debug, PartialEq)]
 struct Vertex {
@@ -34,40 +43,47 @@ impl Vertex {
     }
 }
 
-fn compare_to_line(v_test: &Point, v_prev: &Point, v_next: &Point) -> f32 {
-    // returns negative if test is left of line, positive if right, 0 if colinear
-    (v_test.x - v_prev.x)*(v_next.y - v_prev.y) - (v_test.y - v_prev.y)*(v_next.x - v_prev.x)
+#[derive(Debug, PartialEq)]
+enum LineCompare {
+    Left, Right, On
 }
 
-fn is_convex(v_test: &Point, v_prev: &Point, v_next: &Point) -> bool {
+fn compare_to_line<P, U>(v_test: &P, v_prev: &P, v_next: &P) -> LineCompare 
+    where P :Point<T=U>, U: Mul<Output=U> + Sub<Output=U> + Zeroable + PartialOrd + PartialEq + fmt::Display {
+    let val = (v_test.get_x() - v_prev.get_x())*(v_next.get_y() - v_prev.get_y()) - 
+              (v_test.get_y() - v_prev.get_y())*(v_next.get_x() - v_prev.get_x());
+    let zero = U::zero();
+    if val < zero { LineCompare::Left } else if val > zero { LineCompare::Right } else { LineCompare::On }
+}
+
+fn is_convex<P, U>(v_test: &P, v_prev: &P, v_next: &P) -> bool 
+        where P: Point<T=U>, U: Mul<Output=U>, U: Sub<Output=U>, U: PartialOrd + PartialEq + Sub + Mul + Zeroable + fmt::Display {
     // point is convex if right of line made by prev->next
-    compare_to_line(v_test, v_prev, v_next) > 0.0f32
+    compare_to_line(v_test, v_prev, v_next) == LineCompare::Right
 }
 
-fn is_in_triangle(v_test: &Point, v0: &Point, v1: &Point, v2: &Point) -> bool {
-    if compare_to_line(v_test, v0, v1) >= 0.0f32 { return false; }
-    if compare_to_line(v_test, v1, v2) >= 0.0f32 { return false; }
-    if compare_to_line(v_test, v2, v0) >= 0.0f32 { return false; }
+fn is_in_triangle<P, U>(v_test: &P, v0: &P, v1: &P, v2: &P) -> bool 
+        where P: Point<T=U>, U: Mul<Output=U>, U: Sub<Output=U>, U: PartialOrd + PartialEq + Sub + Mul + Zeroable + fmt::Display {
+    if compare_to_line(v_test, v0, v1) != LineCompare::Left { return false; }
+    if compare_to_line(v_test, v1, v2) != LineCompare::Left { return false; }
+    if compare_to_line(v_test, v2, v0) != LineCompare::Left { return false; }
     true
 }
 
 // note: this function assumes v_test is convex!
-fn is_ear(points: &Vec<Point>, reflex_set: &HashSet<usize>, v_test: &Vertex) -> bool {
-    println!("is_ear({}), prev={}, next={}", v_test.index, v_test.prev_index, v_test.next_index);
+fn is_ear<P, U>(points: &Vec<P>, reflex_set: &HashSet<usize>, v_test: &Vertex) -> bool 
+        where P: Point<T=U>, U: Mul<Output=U>, U: Sub<Output=U>, U: PartialOrd + PartialEq + Sub + Mul + Zeroable + fmt::Display {
     for r in reflex_set {
         if *r == v_test.prev_index || *r == v_test.next_index {
-            println!("{} is adjacent, doesn't count", r);
             continue;
         }
         if is_in_triangle(&points[*r], 
                           &points[v_test.prev_index], 
                           &points[v_test.index], 
                           &points[v_test.next_index]) {
-            println!("{} is in triangle {},{},{}, not an ear", r, v_test.prev_index, v_test.index, v_test.next_index);
             return false;
         }
     }
-    println!("{},{},{}, is an ear", v_test.prev_index, v_test.index, v_test.next_index);
     true
 }
 
@@ -88,7 +104,8 @@ enum VertexType {
     Ear
 }
 
-fn classify_vertex(points: &Vec<Point>, v_test: &mut Vertex, reflex_set: &HashSet<usize>) -> VertexType {
+fn classify_vertex<P, U>(points: &Vec<P>, v_test: &mut Vertex, reflex_set: &HashSet<usize>) -> VertexType
+        where P: Point<T=U>, U: Mul<Output=U>, U: Sub<Output=U>, U: PartialOrd + PartialEq + Sub + Mul + Zeroable + fmt::Display {
     if is_convex(&points[v_test.index], &points[v_test.prev_index], &points[v_test.next_index]) {
         if is_ear(&points, reflex_set, &v_test) {
              return VertexType::Ear;
@@ -100,7 +117,8 @@ fn classify_vertex(points: &Vec<Point>, v_test: &mut Vertex, reflex_set: &HashSe
     }
 }
 
-fn fill_sets(points: &Vec<Point>, vertices: &mut Vec<Vertex>) -> (HashSet<usize>, HashSet<usize>) {
+fn fill_sets<P, U>(points: &Vec<P>, vertices: &mut Vec<Vertex>) -> (HashSet<usize>, HashSet<usize>)
+        where P: Point<T=U>, U: Mul<Output=U>, U: Sub<Output=U>, U: PartialOrd + PartialEq + Sub + Mul + Zeroable + fmt::Display {
     let mut ear_set = HashSet::new();
     let mut reflex_set = HashSet::new();
 
@@ -141,7 +159,8 @@ fn push_triangle(triangles: &mut Vec<usize>, i_test: usize, i_prev: usize, i_nex
     triangles.push(i_next);
 }
 
-pub fn triangulate(points: &Vec<Point>) -> Result<Vec<usize>, &'static str> {
+pub fn triangulate<P, U>(points: &Vec<P>) -> Result<Vec<usize>, &'static str>
+         where P: Point<T=U>, U: Mul<Output=U>, U: Sub<Output=U>, U: PartialOrd + PartialEq + Sub + Mul + Zeroable + fmt::Display {
     let mut n = points.len();
     if n < 4 {
         if n == 3 {
@@ -234,62 +253,139 @@ pub fn triangulate(points: &Vec<Point>) -> Result<Vec<usize>, &'static str> {
         }
     }
 }
+
+fn triangle_edges(v0: usize, v1: usize, v2: usize, max: usize) -> (bool, bool, bool) { 
+    let e0 = v1 == 0 && v0 == max || (v1 > v0 && v1 - v0 == 1);
+    println!("{} -> {}: {}", v0, v1, e0);
+    let e1 = v2 == 0 && v1 == max || (v2 > v1 && v2 - v1 == 1);
+    println!("{} -> {}: {}", v1, v2, e1);
+    let e2 = v0 == 0 && v2 == max || (v0 > v2 && v0 - v2 == 1);
+    println!("{} -> {}: {}", v2, v0, e2);
+    (e0, e1, e2)
+}
+ 
+pub fn find_edges(triangles: &Vec<usize>, max: usize) -> Vec<bool> {
+    let n = triangles.len();
+    let mut edges = Vec::with_capacity(n);
+
+    for i in 0..(n/3) {
+        let e = triangle_edges(triangles[i*3], triangles[i*3+1], triangles[i*3+2], max);
+        edges.push(e.0);
+        edges.push(e.1);
+        edges.push(e.2);
+    }
+    edges
+}
+
+extern crate libc;
+
+use libc::size_t;
+use libc::c_float;
+use libc::c_uchar;
+use std::ptr;
+
+impl Zeroable for f32 {
+    fn zero() -> f32 { 0.0f32 }
+}
+
+#[no_mangle]
+pub extern "C" fn c_triangulate(num_points: size_t, c_points: *const c_float, 
+                                num_triangle_indices: size_t, c_triangle_indices: *mut size_t,
+                                num_edges: size_t, c_edges: *mut c_uchar) -> i32 {
+    if num_points % 3usize != 0usize {
+        return -1i32;
+    }
+    if num_triangle_indices < 3usize * (num_points - 2usize) {
+        return -1i32;
+    }
+    if num_edges != num_triangle_indices {
+        return -1i32;
+    }
+    if c_points == ptr::null() || c_triangle_indices == ptr::null_mut() || c_edges == ptr::null_mut() {
+        return -2i32;
+    }
+
+    unsafe {
+        let points_slice = std::slice::from_raw_parts(c_points, num_points*2);
+        let mut points = Vec::with_capacity(num_points);
+        for i in 0..num_points {
+            points.push((points_slice[2*i], points_slice[2*i+1]));
+        }
+
+        match triangulate(&points) {
+            Ok(triangle_indices) => {
+                let edges = find_edges(&triangle_indices, num_points);
+                let triangle_slice = std::slice::from_raw_parts_mut(c_triangle_indices, num_triangle_indices);
+                let edge_slice = std::slice::from_raw_parts_mut(c_edges, num_edges);
+                for i in 0..num_triangle_indices {
+                    triangle_slice[i] = triangle_indices[i];
+                    edge_slice[i] = if edges[i] { 1u8 } else { 08 }
+                }
+                0i32
+            },
+            Err(_) => -3i32
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::Point;
+    use super::Zeroable;
     use super::compare_to_line;
     use super::is_convex;
     use super::is_in_triangle;
     use super::triangulate;
+    use super::LineCompare;
+    use super::find_edges;
+
+    impl Zeroable for f32 {
+        fn zero() -> f32 { 0.0f32 }
+    }
 
     #[test]
     fn test_compare_to_line() {
-        let test = Point::new(0.2f32, 1.2f32);
-        let v0 = Point::new(0.1f32,  0.1f32);
-        let v1 = Point::new(0.5f32, 0.5f32);
+        let v0 = (0.1f32,  0.1f32);
+        let v1 = (0.5f32, 0.5f32);
 
-        assert!(compare_to_line(&test, &v0, &v1) < 0.0f32);
+        let test = (0.2f32, 1.2f32);
+        assert_eq!(compare_to_line(&test, &v0, &v1), LineCompare::Left);
         
-        let test = Point::new(0.2f32, -1.0f32);
-
-        assert!(compare_to_line(&test, &v0, &v1) > 0.0f32);
+        let test = (0.2f32, -1.0f32);
+        assert_eq!(compare_to_line(&test, &v0, &v1), LineCompare::Right);
         
-        let test = Point::new(v0.x + (v1.x - v0.x) * 0.2f32, v0.y + (v1.y - v0.y) * 0.2f32);
-        
-        assert_eq!(compare_to_line(&test, &v0, &v1), 0.0f32);
+        let test = (v0.0 + (v1.0 - v0.0) * 0.2f32, v0.1 + (v1.1 - v0.1) * 0.2f32);      
+        assert_eq!(compare_to_line(&test, &v0, &v1), LineCompare::On);
     }
     
     #[test]
     fn test_is_convex() {
-        let test = Point::new(0.2f32, 1.2f32);
-        let v0 = Point::new(0.1f32, 0.1f32);
-        let v1 = Point::new(0.5f32, 0.5f32);
+        let v0 = (0.1f32, 0.1f32);
+        let v1 = (0.5f32, 0.5f32);
 
-        assert_eq!(is_convex(&test, &v0, &v1), false);
+        let test = (0.2f32, 1.2f32);
+        assert!(!is_convex(&test, &v0, &v1));
         
-        let test = Point::new(0.2f32, -1.0f32);
-
+        let test = (0.2f32, -1.0f32);
         assert!(is_convex(&test, &v0, &v1));
         
-        let test = Point::new(v0.x + (v1.x - v0.x) * 0.2f32, v0.y + (v1.y - v0.y) * 0.2f32);
-        
-        assert_eq!(is_convex(&test, &v0, &v1), false);
+        let test = (v0.0 + (v1.0 - v0.0) * 0.2f32, v0.1 + (v1.1 - v0.1) * 0.2f32);
+        assert!(!is_convex(&test, &v0, &v1));
     }
     
     #[test]
     fn test_is_in_triangle() {
-        let v0 = Point::new(0.1f32, 0.1f32);
-        let v1 = Point::new(0.7f32, -0.2f32);
-        let v2 = Point::new(0.5f32, 0.5f32);
+        let v0 = (0.0f32, 0.0f32);
+        let v1 = (1.0f32, 0.0f32);
+        let v2 = (0.0f32, 1.0f32);
 
-        let test = Point::new(0.2f32, 1.2f32);
-        assert_eq!(is_in_triangle(&test, &v0, &v1, &v2), false);
+        let test = (0.75f32, 0.75f32);
+        assert!(!is_in_triangle(&test, &v0, &v1, &v2));
         
-        let test = Point::new(0.4f32, 0.2f32);
+        let test = (0.25f32, 0.25f32);
         assert!(is_in_triangle(&test, &v0, &v1, &v2));
         
-        let test = Point::new(v2.x + (v0.x - v2.x) * 0.25f32, v2.y + (v0.y - v2.y) * 0.25f32);
-        assert_eq!(is_in_triangle(&test, &v0, &v1, &v2), false);
+        let test = (0.5f32, 0.5f32);
+        assert!(!is_in_triangle(&test, &v0, &v1, &v2));
     }
 
     fn is_expected_triangle(v0: &usize, v1: &usize, v2: &usize, expected: &(usize, usize, usize)) -> bool {
@@ -332,10 +428,10 @@ mod tests {
 
     #[test]
     fn test_triangulate_square() {
-        let points = vec![ Point::new(0.0f32, 0.0f32),
-                           Point::new(1.0f32, 0.0f32), 
-                           Point::new(1.0f32, 1.0f32), 
-                           Point::new(0.0f32, 1.0f32) ];
+        let points = vec![ (0.0f32, 0.0f32),
+                           (1.0f32, 0.0f32), 
+                           (1.0f32, 1.0f32), 
+                           (0.0f32, 1.0f32) ];
 
         let triangles = triangulate(&points).unwrap();
 
@@ -351,11 +447,11 @@ mod tests {
 
     #[test]
     fn test_triangulate_reflex() {
-        let points = vec![ Point::new(0.0f32, 0.0f32),
-                            Point::new(5.0f32, 0.0f32), 
-                            Point::new(2.0f32, 2.0f32), 
-                            Point::new(5.0f32, 4.0f32),
-                            Point::new(0.0f32, 4.0f32) ];
+        let points = vec![ (0.0f32, 0.0f32),
+                           (5.0f32, 0.0f32), 
+                           (2.0f32, 2.0f32), 
+                           (5.0f32, 4.0f32),
+                           (0.0f32, 4.0f32) ];
 
         let triangles = triangulate(&points).unwrap();
 
@@ -364,5 +460,25 @@ mod tests {
         }
 
         assert!(is_same_triangulation(&triangles, vec![(0, 1, 2), (0, 2, 4), (4, 2, 3)]));
+    }
+
+    #[test]
+    fn test_find_edges() {
+        // for now I will just inspect the output
+        let points = vec![ (0.0f32, 0.0f32),
+                           (5.0f32, 0.0f32), 
+                           (2.0f32, 2.0f32), 
+                           (5.0f32, 4.0f32),
+                           (0.0f32, 4.0f32) ];
+
+        let triangles = triangulate(&points).unwrap();
+        let edges = find_edges(&triangles, 4);
+
+        for i in 0..(triangles.len() / 3) {
+            println!("T {}, {}, {}", triangles[i*3], triangles[i*3+1], triangles[i*3+2]);
+            println!("E {}, {}, {}", edges[i*3], edges[i*3+1], edges[i*3+2]);
+        }
+
+
     }
 }
