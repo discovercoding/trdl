e0cdb320f0188dee27b791e30db1ffa2c515ab66
JeremySorensen <jeremy.a.sorensen@gmail.com>
2016-11-14 08:01:09 -0800

    intermediate commit
    just so I can work on stuff somewhere else

--- src/bin.rs
+++ src/bin.rs
@@ -3,8 +3,20 @@ extern crate trdl;
 
 use std::os::raw::c_void;
 
-fn make_shape(off_x: f32, off_y: f32) -> Vec<trdl::Path> {
+fn off(point: (f32, f32), offset: (f32, f32)) -> (f32, f32) {
+    (point.0 + offset.0, point.1 + offset.1)
+}
 
+fn make_shape(off_x: f32, off_y: f32) -> Vec<trdl::Path> {
+    let o = (off_x, off_y);
+    let mut shapes = Vec::new();
+    let foot = trdl::Path::new(off((200f32, -100f32), o)).
+        line_to(off((100f32, -200f32), o)).
+        curve_to(off((125f32, -300f32), o), off((200f32, -350f32), o), off((300f32, -300f32), o)).
+        curve_to(off((280f32, -200f32), o), off((250f32, -150f32), o), off((200f32, -100f32), o)).
+        close_path().set_stroke(0.0f32, 0.0f32, 0.0f32, 2).set_fill_color(0.0f32, 1.0f32, 0.0f32);
+    shapes.push(foot);
+    shapes
 }
 
 struct Window {
@@ -43,21 +55,29 @@ fn main() {
         (0.0f32, 0.0f32, 0.3f32), (0.3f32, 0.7f32, 0.0f32)];
     let thicknesses = [5, 10, 20, 50];
 
-    let mut do_fill = true;
-    for i in 0..sqrt_size {
-        let delta_x = 100 + wx * (i as i32) / (sqrt_size as i32);
-        for j in 0..sqrt_size {
-            let delta_y = 100 + wy * (j as i32) / (sqrt_size as i32);
-            let fill_color = if do_fill { Some(colors[idx]) } else { None };
-            let (s1, s2) = make_shape(delta_x as f32, delta_y as f32,
-                                      fill_color, stroke_colors[idx], thicknesses[idx]);
-            drawing.add_path(s1).unwrap();
-            drawing.add_path(s2).unwrap();
-            do_fill = !do_fill;
-            idx += 1;
-            if idx > 3 { idx = 0; }
-        }
+//    let mut do_fill = true;
+//    for i in 0..sqrt_size {
+//        let delta_x = 100 + wx * (i as i32) / (sqrt_size as i32);
+//        for j in 0..sqrt_size {
+//            let delta_y = 100 + wy * (j as i32) / (sqrt_size as i32);
+//            let fill_color = if do_fill { Some(colors[idx]) } else { None };
+//            let shapes = make_shape(delta_x as f32, delta_y as f32);
+//            for s in shapes {
+//                drawing.add_path(s).unwrap();
+//            }
+//            do_fill = !do_fill;
+//            idx += 1;
+//            if idx > 3 { idx = 0; }
+//        }
+//    }
+
+    let delta_x = 400f32;
+    let delta_y = 400f32;
+    let shapes = make_shape(delta_x as f32, delta_y as f32);
+    for s in shapes {
+        drawing.add_path(s).unwrap();
     }
+
     drawing.draw();
     window.w.swap_buffers().unwrap();
     for event in window.w.wait_events() {
